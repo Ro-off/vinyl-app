3ab6ae0fb0c9013fbebb4a9bdb2d5af4f3b2b341
Roman <romansk2001@gmail.com>
2024-02-09 14:28:09 +0200

    Migrate website layout to jsx file and render it in StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Application } from "./Application.jsx";
+import { StrictMode } from "react";
 
 const appElement = document.getElementById("app");
 const root = createRoot(appElement);
 
-root.render(<Application />);
+root.render(
+  <StrictMode>
+    <Application />
+  </StrictMode>
+);
