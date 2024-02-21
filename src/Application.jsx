bb52518e6b06ffa508d99270f1cb2f922ce21398
Roman <romansk2001@gmail.com>
2024-02-21 15:43:54 +0200

    Move layout into jsx variables

--- src/Application.jsx
+++ src/Application.jsx
@@ -26,6 +26,111 @@ export const Application = () => {
     console.error("Favorites not implemented");
   }
 
+  function PageLayout({ children }) {
+    return <div>{children}</div>;
+  }
+
+  function NavigationHeader() {
+    return (
+      <header>
+        <div
+          role="button"
+          id="back-button"
+          tabIndex={0}
+          onClick={window.history.back()}
+        >
+          <img alt="" src="src/assets/icons/chevron_left.svg" />
+          <p>Back</p>
+        </div>
+        <div id="favorites-button">
+          <img alt="Liked" src="src/assets/icons/liked.svg" />
+        </div>
+        <div id="library-button">
+          <img alt="Library" src="src/assets/icons/folder.svg" />
+        </div>
+      </header>
+    );
+  }
+
+  function Filter() {
+    return (
+      <div id="filter">
+        <form action="" id="filter-form">
+          <input type="text" name="artist" id="artist" placeholder="Genre" />
+          <select
+            name="genre"
+            id="genre"
+            title="genre"
+            defaultValue="placeholder"
+          >
+            <option className="filter-placeholder" value="placeholder">
+              Genre
+            </option>
+            <option value="rock">Rock</option>
+            <option value="pop">Pop</option>
+            <option value="country">Country</option>
+            <option value="hip-hop">Hip-Hop</option>
+            <option value="jazz">Jazz</option>
+          </select>
+          <select
+            name="decade"
+            id="decade"
+            title="genre"
+            defaultValue="placeholder"
+          >
+            <option className="filter-placeholder" value="placeholder">
+              Decade
+            </option>
+            <option value="1950-60">1950-60 рр.</option>
+            <option value="1960-70">1960-70 рр.</option>
+            <option value="1970-80">1970-80 рр.</option>
+            <option value="1980-90">1980-90 рр.</option>
+            <option value="1990-00">1990-00 рр.</option>
+            <option value="2000-10">2000-10 рр.</option>
+            <option value="2010-20">2010-20 рр.</option>
+            <option value="2020-30">2020-30 рр.</option>
+          </select>
+          <select
+            name="country"
+            id="country"
+            title="country"
+            defaultValue="placeholder"
+          >
+            <option className="filter-placeholder" value="placeholder">
+              Country
+            </option>
+            <option value="usa">USA</option>
+            <option value="uk">UK</option>
+            <option value="france">France</option>
+            <option value="germany">Germany</option>
+            <option value="ukraine">Ukraine</option>
+          </select>
+          <input type="submit" value="Search" id="filter-search" />
+        </form>
+      </div>
+    );
+  }
+
+  function CardContainer() {
+    return <div id="items-container"> {musicList.map(renderCard)} </div>;
+  }
+
+  function NavigationPages() {
+    return (
+      <div id="pages-container">
+        <div id="pages">
+          <button className="page-id" id="active-page">
+            1
+          </button>
+          <button className="page-id">2</button>
+          <button className="page-id">3</button>
+          <p>...</p>
+          <button className="page-id">10</button>
+        </div>
+      </div>
+    );
+  }
+
   function renderCard({
     itemId,
     name,
@@ -104,92 +209,11 @@ export const Application = () => {
   }
 
   return (
-    <div>
-      <header>
-        <div
-          role="button"
-          id="back-button"
-          tabIndex={0}
-          onClick={window.history.back()}
-        >
-          <img alt="" src="src/assets/icons/chevron_left.svg" />
-          <p>Back</p>
-        </div>
-        <div id="favorites-button">
-          <img alt="Liked" src="src/assets/icons/liked.svg" />
-        </div>
-        <div id="library-button">
-          <img alt="Library" src="src/assets/icons/folder.svg" />
-        </div>
-      </header>
-
-      <div id="filter">
-        <form action="" id="filter-form">
-          <input type="text" name="artist" id="artist" placeholder="Genre" />
-          <select
-            name="genre"
-            id="genre"
-            title="genre"
-            defaultValue="placeholder"
-          >
-            <option className="filter-placeholder" value="placeholder">
-              Genre
-            </option>
-            <option value="rock">Rock</option>
-            <option value="pop">Pop</option>
-            <option value="country">Country</option>
-            <option value="hip-hop">Hip-Hop</option>
-            <option value="jazz">Jazz</option>
-          </select>
-          <select
-            name="decade"
-            id="decade"
-            title="genre"
-            defaultValue="placeholder"
-          >
-            <option className="filter-placeholder" value="placeholder">
-              Decade
-            </option>
-            <option value="1950-60">1950-60 рр.</option>
-            <option value="1960-70">1960-70 рр.</option>
-            <option value="1970-80">1970-80 рр.</option>
-            <option value="1980-90">1980-90 рр.</option>
-            <option value="1990-00">1990-00 рр.</option>
-            <option value="2000-10">2000-10 рр.</option>
-            <option value="2010-20">2010-20 рр.</option>
-            <option value="2020-30">2020-30 рр.</option>
-          </select>
-          <select
-            name="country"
-            id="country"
-            title="country"
-            defaultValue="placeholder"
-          >
-            <option className="filter-placeholder" value="placeholder">
-              Country
-            </option>
-            <option value="usa">USA</option>
-            <option value="uk">UK</option>
-            <option value="france">France</option>
-            <option value="germany">Germany</option>
-            <option value="ukraine">Ukraine</option>
-          </select>
-          <input type="submit" value="Search" id="filter-search" />
-        </form>
-      </div>
-
-      <div id="items-container">{musicList.map(renderCard)} </div>
-      <div id="pages-container">
-        <div id="pages">
-          <button className="page-id" id="active-page">
-            1
-          </button>
-          <button className="page-id">2</button>
-          <button className="page-id">3</button>
-          <p>...</p>
-          <button className="page-id">10</button>
-        </div>
-      </div>
-    </div>
+    <PageLayout>
+      <NavigationHeader />
+      <Filter />
+      <CardContainer />
+      <NavigationPages />
+    </PageLayout>
   );
 };
