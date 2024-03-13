2dff482c85e16c9686deb126229ac393c86ff744
Your Name <romansk2001@gmail.com>
2024-03-13 15:20:49 +0200

    Added Storybook

---
+++ src/stories/Card.stories.jsx
@@ -0,0 +1,25 @@
+import { renderCard } from "../components/Card";
+
+export default {
+  title: "Card",
+  component: renderCard,
+};
+
+const defaultArgs = {
+  itemId: "1",
+  name: "The Name",
+  author: "The Author",
+  genreId: "1",
+  year: "2021",
+  countryId: "1",
+  imageSrc:
+    "https://i.discogs.com/GCNPRSAsHiTRxtsZj1uTFk48NDTNSh3DRt7YrpHUmMc/rs:fit/g:sm/q:90/h:594/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTExNDAw/MjkwLTE1NTE1ODQ1/MzEtODczNi5qcGVn.jpeg",
+};
+
+export const Default = () => renderCard({ ...defaultArgs });
+
+export const LargeName = () =>
+  renderCard({
+    ...defaultArgs,
+    name: "The Very Long Name of this card item that displays on the page",
+  });
---
+++ src/stories/Filter.stories.jsx
@@ -0,0 +1,8 @@
+import { FilterSection } from "../components/Filter";
+
+export default {
+  title: "Filter",
+  component: FilterSection,
+};
+
+export const Default = (args) => <FilterSection {...args} />;
