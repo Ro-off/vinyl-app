78ba1a87189714cbf23342224b0b4b35dd93b73c
Your Name <romansk2001@gmail.com>
2024-04-26 19:40:23 +0300

    refactor: Update YMLCardContainer to show genres

--- src/components/YMLCardsContainer/YMLCardsContainer.jsx
+++ src/components/YMLCardsContainer/YMLCardsContainer.jsx
@@ -1,14 +1,23 @@
 import styles from "./YMLCardsContainer.module.css";
 import { YMLCard } from "./YMLCard/YMLCard";
+import { useGenres } from "../../hooks/useGenres";
 
 export function YMLCardsContainer() {
+  const genres = useGenres();
   return (
     <div className={styles.container}>
       <h2>You may like</h2>
-      <div className={styles.cards}>
-        <YMLCard title="Rock" genreId={4} background="rgb(235, 82, 64)" />
-        <YMLCard title="Classic" genreId={2} background="rgb(255, 182, 64)" />
-        <YMLCard title="Hip-hop" genreId={1} background="rgb(155, 182, 64)" />
+      <div className={styles.cardScrollContainer}>
+        <div className={styles.cards}>
+          {genres.data.map((genre) => (
+            <YMLCard
+              key={genre.id}
+              title={genre.title}
+              genreId={genre.id}
+              background="rgb(155, 182, 64)"
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
