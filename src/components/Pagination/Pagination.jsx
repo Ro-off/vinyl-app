5db804e0dcc6077934e2a6d6c92657d6170badcd
Your Name <romansk2001@gmail.com>
2024-04-29 21:20:34 +0300

    refactor: Update Pagination component to display ellipsis for page skipping adn render only part of all pages count

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -3,21 +3,32 @@ import PropTypes from "prop-types";
 import styles from "./Pagination.module.css";
 
 export function Pagination({ currentPage, onPageChange, pagesCount }) {
+  function pageButton(i) {
+    return (
+      <button
+        key={i}
+        className={clsx(
+          styles.pageId,
+          i + 1 == currentPage && styles.activePage
+        )}
+        onClick={() => onPageChange(i + 1)}
+      >
+        {i + 1}
+      </button>
+    );
+  }
+
   return (
     <div className={styles.pagesContainer}>
       <div className={styles.pages}>
-        {Array.from({ length: pagesCount }, (_, i) => (
-          <button
-            key={i}
-            className={clsx(
-              styles.pageId,
-              i + 1 == currentPage && styles.activePage
-            )}
-            onClick={() => onPageChange(i + 1)}
-          >
-            {i + 1}
-          </button>
-        ))}
+        {/* {Array.from({ length: pagesCount }, (_, i) => pageButton(i))} */}
+        {currentPage - 2 > 0 ? pageButton(0) : null}
+        {currentPage - 3 > 0 ? <p>...</p> : null}
+        {currentPage > 1 ? pageButton(currentPage - 2) : null}
+        {pageButton(currentPage - 1)}
+        {currentPage < pagesCount ? pageButton(currentPage) : null}
+        {currentPage + 2 < pagesCount ? <p>...</p> : null}
+        {currentPage + 1 < pagesCount ? pageButton(pagesCount - 1) : null}
       </div>
     </div>
   );
