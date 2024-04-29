c41fc564e4b3f3530ab7e47d5492e89944d6cc58
Your Name <romansk2001@gmail.com>
2024-04-29 20:14:55 +0300

    feat: Add "Add to collection" button to VinylModal component

--- src/components/VinylModal/VinylModal.jsx
+++ src/components/VinylModal/VinylModal.jsx
@@ -1,5 +1,6 @@
 import styles from "./VinylModal.module.css";
 import { createPortal } from "react-dom";
+import { MainButton } from "../buttons/MainButton/MainButton";
 
 export function VinylModal() {
   return createPortal(
@@ -26,6 +27,13 @@ export function VinylModal() {
           <p>Country:</p> <p>USA</p>
         </div>
       </div>
+      <MainButton
+        className={styles.collectionButton}
+        defaultChildren="Add to collection"
+        onActiveChildren={"In collection"}
+        onClick={() => console.log("Add to collection")}
+        isActive={true}
+      />
     </div>,
     document.getElementById("app")
   );
