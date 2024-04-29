bfba56b31a535a6b797bfa3c015d18504d703ebe
Your Name <romansk2001@gmail.com>
2024-04-30 00:49:18 +0300

    refactor: Update MainButton component with animated child span and dynamic class names

--- src/components/buttons/MainButton/MainButton.jsx
+++ src/components/buttons/MainButton/MainButton.jsx
@@ -1,7 +1,32 @@
 import styles from "./MainButton.module.css";
 import PropTypes from "prop-types";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import clsx from "clsx";
+
+function buttonChildrenSpan(isActive, children) {
+  return (
+    <AnimatePresence>
+      {isActive && (
+        <motion.span
+          // variants={childrenVariants}
+          initial={{ opacity: 1 }}
+          animate={{
+            opacity: 1,
+            filter: "drop-shadow(0 0 0 transparent)",
+          }}
+          exit={{
+            opacity: 0,
+            filter:
+              "drop-shadow(0 0 1px var(--text-1)) drop-shadow(0 0 1px var(--text-1)) drop-shadow(0 0 1px var(--text-1)) drop-shadow(0 0 1px var(--text-1))",
+          }}
+          className={styles.buttonChildrenContainer}
+        >
+          {children}
+        </motion.span>
+      )}
+    </AnimatePresence>
+  );
+}
 
 export function MainButton(props) {
   const {
@@ -12,7 +37,7 @@ export function MainButton(props) {
     className,
   } = props;
 
-  const variants = {
+  const buttonVariants = {
     active: { background: "var(--accent-text)" },
     inactive: { background: "var(--accent-color)" },
   };
@@ -25,14 +50,11 @@ export function MainButton(props) {
       initial={{ scale: 1 }}
       whileTap={{ scale: 0.9 }}
       whileHover={{ cursor: "pointer", filter: "brightness(1.1)" }}
-      variants={variants}
+      variants={buttonVariants}
       animate={isActive ? "active" : "inactive"}
     >
-      {!isActive
-        ? defaultChildren
-        : onActiveChildren
-        ? onActiveChildren
-        : defaultChildren}
+      {buttonChildrenSpan(isActive, defaultChildren)}
+      {buttonChildrenSpan(!isActive, onActiveChildren)}
     </motion.button>
   );
 }
