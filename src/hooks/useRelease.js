1f1d6eb0d1632d1dcfe4698b1ab013b1a5a4b3a4
Your Name <romansk2001@gmail.com>
2024-05-01 15:12:52 +0300

    refactor: Add useRelease hook for fetching release data

---
+++ src/hooks/useRelease.js
@@ -0,0 +1,47 @@
+import useSWR from "swr";
+import { useGenres } from "./useGenres";
+import { useCountries } from "./useCountries";
+
+function fetcher(url) {
+  return fetch(url).then((res) => res.json());
+}
+
+export function useRelease(props) {
+  const genres = useGenres();
+  const countries = useCountries();
+
+  const { releaseId } = props;
+
+  const { data, isLoading, error } = useSWR(["/api/releases/", releaseId], () =>
+    fetcher("/api/releases/" + releaseId, {
+      suspense: true,
+    })
+  );
+
+  return {
+    data:
+      isLoading || error
+        ? []
+        : {
+            genre: genres.isLoading
+              ? "Loading"
+              : String(
+                  genres.data.find(
+                    (element) => element.id == data.release.genre
+                  ).title
+                ),
+            country: genres.isLoading
+              ? "Loading"
+              : String(
+                  countries.data.find(
+                    (element) => element.id == data.release.country
+                  ).title
+                ),
+            year: data.release.year,
+            imageSrc: data.release.cover_image,
+            itemId: data.release.id,
+            name: data.release.title,
+            author: data.release.artist,
+          },
+  };
+}
