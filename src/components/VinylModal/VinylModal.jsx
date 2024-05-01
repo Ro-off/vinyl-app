fd0596dc516d2555f8b2d0fceea58c87aa033529
Your Name <romansk2001@gmail.com>
2024-05-01 15:27:05 +0300

    refactor: Add lazy loading for images and useRelease hook in VinylModal component

--- src/components/VinylModal/VinylModal.jsx
+++ src/components/VinylModal/VinylModal.jsx
@@ -9,13 +9,14 @@ import { HeartOutlineIcon } from "../Icon/HeartOutline";
 import { CloseIcon } from "../Icon/Close";
 import { PlayIcon } from "../Icon/Play";
 import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { useState, Suspense } from "react";
+import { useRelease } from "../../hooks/useRelease";
+import { Loader } from "../Loader/Loader";
 
 export function VinylModal(props) {
   const {
     isOpen,
     onClose,
-    music,
     inCollection,
     onToggleCollection,
     inFavorites,
@@ -23,78 +24,84 @@ export function VinylModal(props) {
     itemId,
   } = props;
 
+  const { data } = useRelease({ releaseId: itemId });
+  const music = data;
   const [isPlaying, setIsPlaying] = useState(false);
 
   return createPortal(
     <AnimatePresence>
       {isOpen && (
-        <motion.div
-          className={styles.background}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <div className={styles.modal}>
-            <IconButton
-              className={styles.closeButton}
-              onClick={onClose}
-              defaultChildren={<CloseIcon />}
-            />
-            <div className={styles.topBar}>
-              <h1 className={styles.songName}>{music.name}</h1>
-              <h2 className={styles.artistName}>{music.author}</h2>
-            </div>
-            <div className={styles.playerContainer}>
-              <IconButton
-                className={styles.likeButton}
-                onClick={() => onToggleFavorites(itemId)}
-                defaultChildren={<HeartOutlineIcon />}
-                onActiveChildren={<HeartFilledIcon />}
-                isActive={inFavorites}
-              />
-              <IconButton
-                className={styles.playButton}
-                onClick={() => setIsPlaying(!isPlaying)}
-                defaultChildren={<PlayIcon />}
-                isActive={false}
-                variant="circle"
-              />
-              <img
-                className={styles.image}
-                src={music.imageSrc}
-                alt="Vinyl cover"
-              />
-            </div>
-            <div className={styles.infoContainer}>
-              <div className={styles.infoRow}>
-                <p>Year released:</p> <p>{music.year}</p>
+        <Suspense fallback={<Loader />}>
+          <>
+            <motion.div
+              className={styles.background}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <div className={styles.modal}>
+                <IconButton
+                  className={styles.closeButton}
+                  onClick={onClose}
+                  defaultChildren={<CloseIcon />}
+                />
+                <div className={styles.topBar}>
+                  <h1 className={styles.songName}>{music.name}</h1>
+                  <h2 className={styles.artistName}>{music.author}</h2>
+                </div>
+                <div className={styles.playerContainer}>
+                  <IconButton
+                    className={styles.likeButton}
+                    onClick={() => onToggleFavorites(itemId)}
+                    defaultChildren={<HeartOutlineIcon />}
+                    onActiveChildren={<HeartFilledIcon />}
+                    isActive={inFavorites}
+                  />
+                  <IconButton
+                    className={styles.playButton}
+                    onClick={() => setIsPlaying(!isPlaying)}
+                    defaultChildren={<PlayIcon />}
+                    isActive={false}
+                    variant="circle"
+                  />
+                  <img
+                    className={styles.image}
+                    src={music.imageSrc}
+                    alt="Vinyl cover"
+                  />
+                </div>
+                <div className={styles.infoContainer}>
+                  <div className={styles.infoRow}>
+                    <p>Year released:</p> <p>{music.year}</p>
+                  </div>
+                  <div className={styles.infoRow}>
+                    <p>Genre:</p> <p>{music.genre}</p>
+                  </div>
+                  <div className={styles.infoRow}>
+                    <p>Country:</p> <p>{music.country}</p>
+                  </div>
+                </div>
+                <MainButton
+                  className={styles.collectionButton}
+                  defaultChildren={
+                    <>
+                      <p>Add to collection</p>
+                      <PlusIcon />
+                    </>
+                  }
+                  onActiveChildren={
+                    <>
+                      <p>In collection</p>
+                      <DoneIcon />
+                    </>
+                  }
+                  onClick={() => onToggleCollection(itemId)}
+                  isActive={inCollection}
+                />
               </div>
-              <div className={styles.infoRow}>
-                <p>Genre:</p> <p>{music.genre}</p>
-              </div>
-              <div className={styles.infoRow}>
-                <p>Country:</p> <p>{music.country}</p>
-              </div>
-            </div>
-            <MainButton
-              className={styles.collectionButton}
-              defaultChildren={
-                <>
-                  <p>Add to collection</p>
-                  <PlusIcon />
-                </>
-              }
-              onActiveChildren={
-                <>
-                  <p>In collection</p>
-                  <DoneIcon />
-                </>
-              }
-              onClick={() => onToggleCollection(itemId)}
-              isActive={inCollection}
-            />
-          </div>
-        </motion.div>
+            </motion.div>
+          </>
+        </Suspense>
       )}
     </AnimatePresence>,
     document.getElementById("app")
