13f429692e877e7abd0556789906f7155e227217
Your Name <romansk2001@gmail.com>
2024-05-03 14:43:37 +0300

    refactor: Update IconButton component to use styles instead of motion  for button variant styling

--- src/components/buttons/IconButton/IconButton.jsx
+++ src/components/buttons/IconButton/IconButton.jsx
@@ -4,50 +4,27 @@ import clsx from "clsx";
 import { motion } from "framer-motion";
 
 export function IconButton(props) {
-  const {
-    defaultChildren,
-    onActiveChildren,
-    isActive,
-    onClick,
-    className,
-    variant = "default",
-  } = props;
-
-  const variants = {
-    circle: {
-      borderRadius: "50%",
-    },
-    default: {
-      borderRadius: "5px",
-    },
-  };
+  const { children, isActive, onClick, className, variant = "square" } = props;
 
   return (
     <motion.button
       type="button"
-      className={clsx(styles.iconButton, className)}
+      className={clsx(styles.iconButton, className, styles[variant])}
       title="like-button"
       onClick={onClick}
       initial={{ scale: 1 }}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
-      variants={variants}
-      animate={variant}
     >
-      {!isActive
-        ? defaultChildren
-        : onActiveChildren
-        ? onActiveChildren
-        : defaultChildren}
+      {{ children }}
     </motion.button>
   );
 }
 
-IconButton.propTypes = {
-  defaultChildren: PropTypes.node.isRequired,
-  onActiveChildren: PropTypes.node,
-  isActive: PropTypes.bool.isRequired,
-  onClick: PropTypes.func.isRequired,
+IconButton.PropTypes = {
   className: PropTypes.string,
   variant: PropTypes.oneOf(["circle", "default"]),
+  children: PropTypes.node.isRequired,
+  isActive: PropTypes.bool,
+  onClick: PropTypes.func,
 };
