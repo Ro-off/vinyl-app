0592e63a963cf658144aa4f689a79f501c2b9674
Ro_off <romansk2001@gmail.com>
2024-12-27 21:39:41 +0200

    fix: Comment out ErrorBoundary and Notification imports in Application.jsx for future flexibility

--- src/Application.jsx
+++ src/Application.jsx
@@ -4,9 +4,9 @@ import { useCollection } from "./hooks/useCollection";
 import { useFavorites } from "./hooks/useFavorites";
 import { Suspense } from "react";
 import { Loader } from "./components/Loader/Loader";
-import { ErrorBoundary } from "react-error-boundary";
+//import { ErrorBoundary } from "react-error-boundary";
 import { NotificationContainer } from "./components/Notifications/NotificationContainer";
-import { Notification } from "./components/Notifications/Notification/Notification";
+//import { Notification } from "./components/Notifications/Notification/Notification";
 
 export const Application = () => {
   const { collection, toggleCollection } = useCollection();
@@ -16,16 +16,16 @@ export const Application = () => {
     <>
       <NavigationHeader />
       <Suspense fallback={<Loader />}>
-        <ErrorBoundary fallback={<Notification text={"Error"} type="error" />}>
-          <Outlet
-            context={{
-              collection,
-              toggleCollection,
-              favorites,
-              toggleFavorites,
-            }}
-          />
-        </ErrorBoundary>
+        {/* <ErrorBoundary fallback={<Notification text={"Error"} type="error" />}> */}
+        <Outlet
+          context={{
+            collection,
+            toggleCollection,
+            favorites,
+            toggleFavorites,
+          }}
+        />
+        {/* </ErrorBoundary> */}
       </Suspense>
       <NotificationContainer />
     </>
