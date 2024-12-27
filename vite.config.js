66017c7b548c493ff8e4df3b5152520eead4b8c3
Ro_off <romansk2001@gmail.com>
2024-12-27 22:34:55 +0200

    fix: Implement redirect logic in index.html and add custom 404 page for better navigation

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 });
